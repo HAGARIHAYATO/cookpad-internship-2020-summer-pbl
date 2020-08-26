a98e01cab18ee7785a9b183906cba0358ea2c2ec
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-08-26 13:04:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+- 必要データはなるべく擬似的なデータで持っておく(レシピなど) /public/data.json
+- 言語切替のために文言はjsonで保持 /public/~
+- styled-jsxを使用する。colorは/public/colors.jsonで保持 
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <p>aaaaa</p>
+      </div>
+      <style jsx>{`
+      `}</style>
+    </>
+  );
+}
+
+export default App;
