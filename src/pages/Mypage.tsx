19d0587daea4b3463f9eaa45bb55635bd72774a5
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-08-27 15:22:16 +0900

    modified: mypage

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -3,6 +3,7 @@ import history from '../history';
 import CheckCard from '../component/CheckCard'
 import Carousel from '../component/Carousel'
 import { Props, Recipe } from '../static/index'
+import { userInfo } from 'os';
 
 const Mypage: FC<Props> = (props) => {
   const getRecipe = (ids: Array<number>): Array<Recipe> => {
@@ -29,6 +30,17 @@ const Mypage: FC<Props> = (props) => {
         }}>更新</button>
         <Carousel items={getRecipe(props.user.post_recipe_ids)} />
         <h2 className="container__title">ユーザー情報</h2>
+        <div className="user_container">
+          <h3>{props.user.name}</h3>
+          <br/> 
+          <p><b>レシピ投稿数</b>: {props.user.post_recipe_ids.length} (件)</p>
+          <p><b>貢献ポイント</b>: {props.user.post_recipe_ids.length*5+props.user.favorite_recipe_ids.length*1} (pt)</p>
+          <br/>
+          <p>貢献ポイントとは</p>
+          <br/>
+          <p>サービスへの貢献度をポイントとして表したものです。</p>
+          <p>(投稿数)x5pt, (お気に入り数)x1pt</p>
+        </div>
       </div>
       <style jsx>{`
         .body {
@@ -44,6 +56,15 @@ const Mypage: FC<Props> = (props) => {
           display: flex;
           justify-content: space-around;
           flex-wrap: wrap;
+          padding: 5%;
+          border: solid 1px black;
+        }
+        .user_container {
+          position: relative;
+          border: solid 1px black;
+          width: 60%;
+          padding: 5%;
+          margin: 50px auto;
         }
         .btn {
           margin: 0 auto;
